d2e7228202f7619a33dedbd40aa15ce4b648b81b
Dan Kingston <d@nkingston.com>
2020-09-29 16:02:09 +0100

    Accessibility and disclaimer pages with links from footer

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import CookiesView, CookiePolicyView
 
@@ -7,4 +8,6 @@ app_name = "core"
 urlpatterns = [
     path("cookies/", CookiesView.as_view(), name="cookies"),
     path("cookie-policy/", CookiePolicyView.as_view(), name="cookie-policy"),
+    path('accessibility/', TemplateView.as_view(template_name="pages/accessibility.html"), name="accessibility"),
+    path('disclaimer/', TemplateView.as_view(template_name="pages/disclaimer.html"), name="disclaimer"),
 ]
