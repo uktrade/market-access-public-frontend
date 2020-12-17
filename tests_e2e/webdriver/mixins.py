b0c6d1f1cf1f38ae2d4511f1b53fa3e601d0e2d9
Feri Csepregi <feri@31.digital>
2020-12-17 15:43:15 +0000

    fixup! add auth to url when navigating around the site

--- tests_e2e/webdriver/mixins.py
+++ tests_e2e/webdriver/mixins.py
@@ -8,6 +8,6 @@ class NavigationMixin:
         """ Helper to add basic auth to requests """
         base = settings.BASE_URL.rstrip("/")
         if settings.AUTH_SECRET:
-            base.replace("://", f"://auth_user:{settings.AUTH_SECRET}@")
+            base = base.replace("://", f"://auth_user:{settings.AUTH_SECRET}@")
         url = f"{base}/{path.lstrip('/')}"
         self.visit(url)
