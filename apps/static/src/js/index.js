1b82f840c679a05b5960130a5b2c532129c07d6a
James Pacileo <608868+jamespacileo@users.noreply.github.com>
2022-05-31 11:11:50 +0100

    TSS-219 (#83)
    * TSS-219
    
    * TSS-219
    
    * changed postgres image to 12

--- apps/static/src/js/index.js
+++ apps/static/src/js/index.js
@@ -1,2 +1,6 @@
 import { initAll } from "govuk-frontend/govuk/all";
+var CookiePolicy = require("./modules/cookie-policy");
 initAll();
+
+var cookiePolicy = new CookiePolicy();
+cookiePolicy.initBanner(".app-cookie-banner", ".js-accept-cookie", "cookies");
