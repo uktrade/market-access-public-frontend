9a886d6954f5602189eb9efa73be112b319f5f8b
abarolo <145478290+abarolo@users.noreply.github.com>
2024-04-02 12:06:10 +0100

    TSS-1596: Paas Migration Requirements (#135)
    * Dependencies
    
    * TSS-1596: Pingdom, Copilot, X-ray, Logging
    
    * Formatting and dependency fix
    
    ---------
    
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>

---
+++ apps/pingdom/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+
+PINGDOM_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<pingdom_http_custom_check>
+    <status>{status}</status>
+</pingdom_http_custom_check>\n"""
+
+COMMENT_TEMPLATE = "<!--{comment}-->\n"
+
+
+def pingdom(request):
+    checked = {}
+
+    if all(item[0] for item in checked.values()):
+        return HttpResponse(
+            PINGDOM_TEMPLATE.format(status="OK"), content_type="text/xml"
+        )
+    else:
+        body = PINGDOM_TEMPLATE.format(status="FALSE")
+        for service_result in filter(lambda x: x[0] is False, checked.values()):
+            body += COMMENT_TEMPLATE.format(comment=service_result[1])
+        return HttpResponse(body, status=500, content_type="text/xml")
