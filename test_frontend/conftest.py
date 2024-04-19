7dba4831d44d2dbd130f5781765b8c138bec47b6
abarolo <asmundbekker@DBT000435.local>
2024-04-19 07:55:31 +0100

    clean

--- test_frontend/conftest.py
+++ test_frontend/conftest.py
@@ -6,13 +6,16 @@ import string
 import pytest
 from playwright.sync_api import sync_playwright
 
-AUTH_URL = os.getenv(
-    "TEST_SSO_LOGIN_URL", "http://market-access.local:9880/auth/login/"
-)
+
 BASE_URL = os.getenv(
     "TEST_BASE_FRONTEND_TESTING_URL", "http://market-access.local:9980/"
 )
 HEADLESS = os.getenv("TEST_HEADLESS", "true").lower() == "true"
+
+
+@pytest.fixture
+def base_url():
+    return BASE_URL
 
 
 @pytest.fixture(scope="session")
@@ -67,81 +70,3 @@ def page(context):
     _page = context.new_page()
     _page.goto(BASE_URL, wait_until="domcontentloaded")
     yield _page
-
-
-@pytest.fixture(scope="session")
-def create_test_barrier(page, session_data):
-    def _create_test_barrier(title):
-
-        if session_data["barrier_id"]:
-            return f'{BASE_URL}barriers/{session_data["barrier_id"]}/'
-
-        random_barrier_id = "".join(
-            random.choice(string.ascii_uppercase) for _ in range(5)
-        )
-        random_barrier_name = f"{title} - {datetime.datetime.now().strftime('%d-%m-%Y')} - {random_barrier_id}"
-
-        page.get_by_role("link", name="Report a barrier").click()
-        page.get_by_role("link", name="Start now").click()
-        page.get_by_label("Barrier title").click()
-        page.get_by_label("Barrier title").fill(random_barrier_name)
-        page.get_by_label("Barrier description").click()
-        page.get_by_label("Barrier description").fill("test description")
-        page.get_by_role("button", name="Continue").click()
-
-        page.locator("#status-radio-2").check()
-        page.locator("#status-radio-3").check()
-        page.locator(
-            "#status-date-group-barrier-status-partially_resolved_date_0"
-        ).click()
-        page.locator(
-            "#status-date-group-barrier-status-partially_resolved_date_0"
-        ).fill("04")
-        page.locator(
-            "#status-date-group-barrier-status-partially_resolved_date_1"
-        ).click()
-        page.locator(
-            "#status-date-group-barrier-status-partially_resolved_date_1"
-        ).fill("2023")
-        page.locator("#id_barrier-status-partially_resolved_description").click()
-        page.locator("#id_barrier-status-partially_resolved_description").fill(
-            "ifdshgihsdpihgf"
-        )
-        page.locator("#status-date-group-barrier-status-start_date_0").click()
-        page.locator("#status-date-group-barrier-status-start_date_0").fill("03")
-        page.locator("#status-date-group-barrier-status-start_date_1").click()
-        page.locator("#status-date-group-barrier-status-start_date_1").fill("2024")
-        page.get_by_role("button", name="Continue").click()
-        page.get_by_role("combobox").select_option("TB00016")
-        page.get_by_role("button", name="Continue").click()
-        page.get_by_role("radio").first.check()
-        page.get_by_role("button", name="Continue").click()
-        page.locator("#main_sector_select").select_option(
-            "9538cecc-5f95-e211-a939-e4115bead28a"
-        )
-        page.get_by_text("Add sector").click()
-        page.locator("#sectors_select").select_option(
-            "9738cecc-5f95-e211-a939-e4115bead28a"
-        )
-        page.get_by_text("Other sectors (optional) Add").click()
-        page.locator("#sectors_select").select_option(
-            "9638cecc-5f95-e211-a939-e4115bead28a"
-        )
-        page.get_by_role("button", name="Continue").click()
-        page.get_by_placeholder("Search Company").click()
-        page.locator("#search-companies-button").click()
-        page.get_by_placeholder("Search Company").fill("Test LTD")
-        page.get_by_placeholder("Search Company").press("Enter")
-        page.get_by_text("TEST LIMITEDCompanies House").click()
-        page.get_by_text("Add company").click()
-        page.get_by_role("button", name="Continue").click()
-        page.get_by_label("Goods", exact=True).check()
-        page.get_by_label("Services", exact=True).check()
-        page.get_by_label("Which goods, services or").click()
-        page.get_by_label("Which goods, services or").fill("isfdgihisdhfgidsfg")
-        page.get_by_role("button", name="Continue").click()
-        page.locator("#continue-button").click()
-        session_data["barrier_id"] = page.url.split("/")[-3]
-        return f'{BASE_URL}/barriers/{session_data["barrier_id"]}/'
-
-    return _create_test_barrier
--- test_frontend/test_navigation.py
+++ test_frontend/test_navigation.py
@@ -1,14 +1,10 @@
 from playwright.sync_api import expect
 
-from .utils import clean_full_url
 
-
-def test_find_resolved_barrier(page):
-    page.goto("https://market-access-publicfe-gotham.london.cloudapps.digital/")
+def test_find_by_location_resolved_barrier(page, base_url):
+    page.goto(base_url)
     page.get_by_role("heading", name="What are you looking for?").click()
-    page.locator("p").filter(has_text="Trade barriers").nth(1).click()
     page.get_by_text("Current issues that may").click()
-    page.locator("p").filter(has_text="Resolved trade barriers").click()
     page.get_by_text("Resolved issues which may").click()
     page.get_by_role("link", name="Trade barriers", exact=True).click()
     page.get_by_role("link", name="Find trade barriers by location").click()
@@ -18,15 +14,15 @@ def test_find_resolved_barrier(page):
     page.get_by_role("link", name="Home").click()
 
 
-def test_find_by_location(page):
-    page.goto("https://market-access-publicfe-gotham.london.cloudapps.digital/")
+def test_find_by_location(page, base_url):
+    page.goto(base_url)
     page.get_by_role("link", name="Resolved trade barriers").click()
     page.get_by_role("link", name="Find resolved trade barriers by location").click()
     page.get_by_role("link", name="Home").click()
 
 
-def test_two(page):
-    page.goto("https://market-access-publicfe-gotham.london.cloudapps.digital/")
+def test_two(page, base_url):
+    page.goto(base_url)
     page.get_by_role("link", name="Trade barriers", exact=True).click()
     page.get_by_role("link", name="Find trade barriers by sector").click()
     page.get_by_role("link", name="Advanced engineering (1)").click()
