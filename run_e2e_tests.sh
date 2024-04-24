7a2486b50eccd63b4cdba9801fb92f9ceef194de
abarolo <asmundbekker@DBT000435.local>
2024-04-24 11:20:55 +0100

    Test Updates

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
 
-# get test file name from command line argument
-$target=$1
+# Initialize variables
+target=""
+target_url=""
+is_headless=false # Default is false (not headless)
 
-# activate python virtual environment
+# Function to parse arguments for flexibility
+parse_args() {
+    for arg in "$@"
+    do
+        case $arg in
+            target=*)
+                target="${arg#*=}"
+                shift # Remove once we have processed it
+                ;;
+            target_url=*)
+                target_url="${arg#*=}"
+                shift # Remove once we have processed it
+                ;;
+            --is-headless)
+                is_headless="true"
+                shift # Remove once we have processed it
+                ;;
+            *)
+                # Assume it's the target_url if target is already set
+                if [ -n "$target" ] && [ -z "$target_url" ]; then
+                    target_url="$arg"
+                elif [ -z "$target" ]; then
+                    target="$arg"
+                fi
+                shift
+                ;;
+        esac
+    done
+}
+
+# Parse the input arguments
+parse_args "$@"
+
+# Activate python virtual environment
 source .venv/bin/activate
 
-# install python dependencies if not already installed
-pip install -r requirements-dev.txt
+# Install python dependencies if not already installed
+pip install -r test_frontend/requirements.txt
 
-python -m playwright install
+if [ -z "$target_url" ]; then
+    echo "Error: No target URL specified."
+    exit 1
+fi
 
-ENV_FILE="dmas-pubfe.local.env"
+# Run playwright with pytest
+PWDEBUG=0 BASE_FRONTEND_TESTING_URL="$target_url" TEST_HEADLESS="$is_headless" pytest -c test_frontend/pytest.ini "test_frontend/$target"
 
-# load environment variables
-export $(cat $ENV_FILE | sed 's/#.*//g' | xargs)
-
-# run playwright with pytest
-PWDEBUG=0 pytest test_frontend/$target
-
-# deactivate python virtual environment
+# Deactivate python virtual environment
 deactivate
--- test_frontend/utils.py
+++ test_frontend/utils.py
@@ -1,9 +1,15 @@
+import inspect
 import os
 import re
+import time
+from functools import wraps
+
+import pytest
+from playwright._impl._errors import TimeoutError
 
 
 def get_base_url():
-    return os.getenv("BASE_FRONTEND_TESTING_URL", "http://market-access.local:9880/")
+    return os.getenv("BASE_FRONTEND_TESTING_URL", "http://market-access.local:9980/")
 
 
 def get_text_content_without_line_separators(text_content):
@@ -27,3 +33,45 @@ def clean_full_url(url):
         # If there's no protocol, just clean the URL directly.
         cleaned_url = re.sub(r"/+", "/", url)
     return cleaned_url
+
+
+def retry(tries=3, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff."""
+
+    def deco_retry(f):
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            # Get argument names of the function
+            arg_names = inspect.getfullargspec(f).args
+
+            # Run the function with retries
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except TimeoutError as e:
+                    msg = f"{e}, Retrying in {mdelay} seconds..."
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+
+            # Final attempt
+            return f(*args, **kwargs)
+
+        # If the function expects a fixture named 'request', assume it's a pytest test
+        if "request" in inspect.getfullargspec(f).args:
+            # Use pytest's request fixture to modify the function with retry logic
+            @pytest.fixture
+            def wrapper(request, *args, **kwargs):
+                # Modify the test function by injecting the retry decorator
+                return f_retry(*args, **kwargs)
+
+            return wrapper
+        else:
+            return f_retry
+
+    return deco_retry
