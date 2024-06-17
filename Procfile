6097a74ffa24ddc898673fa727a037e9ef66a52c
Feroze Rub <feroze@taridigital.com>
2024-06-17 18:01:18 +0100

    Gov PaaS  migration to DBT PaaS (#142)
    * TSS-1596: Paas Migration Requirements (#135)
    
    * Dependencies
    
    * TSS-1596: Pingdom, Copilot, X-ray, Logging
    
    * Formatting and dependency fix
    
    ---------
    
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    
    * Add copilot configuration files
    
    * Kick off build
    
    * Trigger another deploy
    
    * Use correct DB environment variable
    
    * Bump copilot python helper
    
    * Allow ELB health checks
    
    * Add APM to Sentry
    
    * Trigger pipeline
    
    * chore: change portry.lock
    
    * Use env.bool to check whether Elastic APM is enabled
    
    * Merge master
    
    * Formatting of copilot config
    
    ---------
    
    Co-authored-by: abarolo <145478290+abarolo@users.noreply.github.com>
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    Co-authored-by: Emile Swarts <emile.swarts@digital.trade.gov.uk>
    Co-authored-by: Gareth Pitt-Nash <gareth.pitt-nash@digital.trade.gov.uk>
    Co-authored-by: santinomolinaro <santino.molinaro@mail>

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -c config/gunicorn.py config.wsgi:application --bind 0.0.0.0:$PORT --worker-class gevent --worker-connections 1000 --log-file -
+web: opentelemetry-instrument gunicorn -c config/gunicorn.py config.wsgi:application --bind 0.0.0.0:$PORT --worker-class gevent --worker-connections 1000 --log-file -
--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 
+from apps.pingdom.urls import urlpatterns as pingdom_urlpatterns
+
 urlpatterns = []
 
 if settings.DEBUG:
@@ -47,4 +49,4 @@ urlpatterns += [
     path("", include("apps.core.urls", namespace="core")),
     path("", include("apps.feedback.urls", namespace="feedback")),
     path("", include("apps.healthcheck.urls", namespace="healthcheck")),
-]
+] + pingdom_urlpatterns
