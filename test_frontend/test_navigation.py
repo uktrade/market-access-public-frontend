13cd8d3a0536d549a2107f36ad267879391e8832
Uka Osim <uka.osim@digital.trade.gov.uk>
2024-08-01 15:37:34 +0100

    fixing end to end test suit

--- test_frontend/test_navigation.py
+++ test_frontend/test_navigation.py
@@ -1,18 +1,26 @@
 from playwright.sync_api import expect
 
+
 def test_find_unresolved_barrier_links(page, base_url):
     page.goto(base_url)
     page.get_by_role("link", name="Trade barriers", exact=True).click()
-    expect(page.locator("#main-content")).to_contain_text("Find trade barriers by location")
-    expect(page.locator("#main-content")).to_contain_text("Find trade barriers by sector")
-
+    expect(page.locator("#main-content")).to_contain_text(
+        "Find trade barriers by location"
+    )
+    expect(page.locator("#main-content")).to_contain_text(
+        "Find trade barriers by sector"
+    )
 
 
 def test_find_resolved_barrier_links(page, base_url):
     page.goto(base_url)
     page.get_by_role("link", name="Resolved trade barriers", exact=True).click()
-    expect(page.locator("#main-content")).to_contain_text("Find resolved trade barriers by location")
-    expect(page.locator("#main-content")).to_contain_text("Find resolved trade barriers by sector")
+    expect(page.locator("#main-content")).to_contain_text(
+        "Find resolved trade barriers by location"
+    )
+    expect(page.locator("#main-content")).to_contain_text(
+        "Find resolved trade barriers by sector"
+    )
 
 
 def test_find_trade_barrier_choose_location(page, base_url):
